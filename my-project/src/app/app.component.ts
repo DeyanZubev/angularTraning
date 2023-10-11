e51c0317814a0365432beb50d9a066e96a2ee946
Deyan Zubev <zubevdeyan@gmail.com>
2023-10-11 13:46:36 +0300

    angular project just start it

---
+++ my-project/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'my-project';
+
+  ngOnInit(): void {
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+  }
+}
